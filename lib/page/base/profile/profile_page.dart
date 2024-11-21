9f01781df09fe5b9c8a929f7991be4084521ba36
nraliyaynie <B032320074@student.utem.edu.my>
2024-11-21 21:56:36 +0800

    gg

--- lib/page/base/profile/profile_page.dart
+++ lib/page/base/profile/profile_page.dart
@@ -2,7 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
+import '../../../utils/colors.dart';
+import 'edit_profile.dart';
+
 class ProfilePage extends StatelessWidget {
+  get headingTextStyle9 => null;
+  get headingTextStyle => null;
+
   Future<void> _logout(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.clear(); // Clear all data from SharedPreferences
@@ -13,30 +20,198 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.person, size: 100, color: Colors.blue),
-          SizedBox(height: 20),
-          Text(
-            'View your profile details here!',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () => _logout(context), // Call logout method
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Container(
+                  color: AppColors.primary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // Profile Image
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            CircleAvatar(
+                              radius: 70, // Size of the profile image
+                              backgroundImage: AssetImage('assets/profile_image.png'),
+                              backgroundColor: Colors.white, //
+                            ),
+                            const SizedBox(height: 10),
+                            //username
+                            Text(
+                              '@ayunies',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                              overflow: TextOverflow.ellipsis, // kalau nama panjang
+                            ),
+                          ],
+                        ),
+
+
+                        Spacer(),
+
+                        // Follower and post
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Row for Post and Followers
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // Column for Posts
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      'Post',
+                                      style: TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 20,
+                                        fontFamily: 'Montserrat',
+                                      ),
+                                    ),
+                                    Text(
+                                      '10',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Montserrat',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(width: 40),
+
+                                // Column for Followers
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      'Followers',
+                                      style: TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 20,
+                                        fontFamily: 'Montserrat',
+                                      ),
+                                    ),
+                                    Text(
+                                      '120',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Montserrat',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            // Row for Edit Profile and Share Profile Buttons
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => EditProfilePage()),
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: AppColors.primary,
+                                    backgroundColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Edit Profile',
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.primary,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Add the share profile action here
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: AppColors.primary,
+                                    backgroundColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Share Profile',
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.primary,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        Spacer(),
+
+                        // Badge Frame
+                        Container(
+                          width: 100,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'badge',
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ),
+                ),
               ),
-            ),
-            child: Text(
-              'Log Out',
-              style: TextStyle(fontSize: 18, color: Colors.white),
-            ),
+
+              Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ],
       ),
